5f3f6ad617b93e963a381fa126432b8a44f89772
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-01-18 19:38:39 +0300

    dawdawwda

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,102 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header class="header px-3 d-flex align-items-center">
+      <div class="d-flex justify-content-between w-100">
+        <SystemIcons :name="'logo'" :width="130"></SystemIcons>
+        <div class="d-flex">
+          <b-button variant="success" class="mr-2">
+            <span class="button-text">Create order</span>
+            <font-awesome-icon :icon="['far', 'plus-square']" />
+          </b-button>
+          <b-button variant="success">
+            <font-awesome-icon :icon="['far', 'moon']" />
+          </b-button>
+        </div>
+      </div>
+    </header>
+    <b-container fluid tag="main" class="content">
+      <b-row>
+        <b-col>
+          <div class="block block-rounded">
+            <div class="block-header d-flex">
+              <h3 class="block-title">CURRENT ORDER</h3>
+              <div class="block-options d-flex">awddwa</div>
+            </div>
+            <div class="block-content block-content-full">
+              <b-row>
+                <b-col>
+                  <div class="inner-signs">
+                    <span class="text-muted">Addres</span>
+                    <a target="_blank" href="https://yandex.ru/maps/?ll=30.310182,59.951059&z=12&l=map">Карта Санкт-Петербурга</a>
+                    <p>awawdawdwad</p>
+                  </div>
+                  <div class="inner-signs">
+                    <span>Addres</span>
+                    <p>awawdawdwad</p>
+                  </div>
+                  <div class="inner-signs">
+                    <span>Addres</span>
+                    <p>awawdawdwad</p>
+                  </div>
+                </b-col>
+                <b-col>
+                  <div class="inner-signs">
+                    <span>Addres</span>
+                    <p>awawdawdwad</p>
+                  </div>
+                  <div class="inner-signs">
+                    <span>Addres</span>
+                    <p>awawdawdwad</p>
+                  </div>
+                  <div class="inner-signs">
+                    <span>Addres</span>
+                    <p>awawdawdwad</p>
+                  </div>
+                </b-col>
+              </b-row>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
+
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+    };
+  },
+  mounted() {
+    console.log(process.env.VUE_APP_SOMEKEY);
+  }
+};
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.header {
+  height: 60px;
+  background-color: rgba(225, 225, 225, 0.75);
+  & > .main-title {
+  }
+  & .button-text {
+    margin-right: 0.5rem;
+  }
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+@media (max-width: 480px) {
+  .header {
+    & .button-text {
+      display: none;
     }
   }
+}
+.content {
+  padding: 20px 0 0 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import { BootstrapVue } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+
+import SystemIcons from "./components/SystemIcons.vue"
+
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(fas, far)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("SystemIcons", SystemIcons)
+
+
+import style from '@/styles/app.scss'
 
 new Vue({
   router,
