37bc41682d290c4b762c18128a96ee9ffe20ac48
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-01-27 18:04:35 +0300

    change vue.config

---
+++ precache-manifest.57b922d725f296c3829571330cb1e7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a3fb7c48787f3cbc7e",
+    "url": "/css/app.24dfbc64.css"
+  },
+  {
+    "revision": "9f3c2eacfa0a32db22f3807f0f5c9a9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12a3fb7c48787f3cbc7e",
+    "url": "/js/app.91d7fac9.js"
+  },
+  {
+    "revision": "08859a10d6b41ab9e4bc",
+    "url": "/js/chunk-vendors.63dac69a.js"
+  },
+  {
+    "revision": "4f692f6bdf21a332424b52af3865a4e6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
