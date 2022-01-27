6a3ad95ebef7d3f2a1895d5312ddbeea843979e6
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-01-27 18:12:01 +0300

    fix change vue-config

---
+++ dist/precache-manifest.ee3d50e7f5899b1c51369c05aba38055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1f62b54a742b839a54",
+    "url": "/JustCleaning/css/app.24dfbc64.css"
+  },
+  {
+    "revision": "c672b0cd85c69961d04dc3ffd1fb9288",
+    "url": "/JustCleaning/index.html"
+  },
+  {
+    "revision": "dc1f62b54a742b839a54",
+    "url": "/JustCleaning/js/app.37bd5202.js"
+  },
+  {
+    "revision": "defce377d454f4cef4e7",
+    "url": "/JustCleaning/js/chunk-vendors.e88b8801.js"
+  },
+  {
+    "revision": "4f692f6bdf21a332424b52af3865a4e6",
+    "url": "/JustCleaning/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JustCleaning/robots.txt"
+  }
+]);
