b2ce8709c711f45472aa8560fd2053c0003d9de5
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-03-18 15:59:20 +0300

    New wersion

---
+++ precache-manifest.6e80e76bd8aa9f6385eea9b2aba09c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fcdc43e8c5bb430055",
+    "url": "/JustCleaning/css/app.9739c5df.css"
+  },
+  {
+    "revision": "86ed1ae660976420b663",
+    "url": "/JustCleaning/css/chunk-vendors.f579dc58.css"
+  },
+  {
+    "revision": "d415fa511a02116e23f9ad22cf29dc48",
+    "url": "/JustCleaning/index.html"
+  },
+  {
+    "revision": "09fcdc43e8c5bb430055",
+    "url": "/JustCleaning/js/app.a59ca951.js"
+  },
+  {
+    "revision": "86ed1ae660976420b663",
+    "url": "/JustCleaning/js/chunk-vendors.88383e26.js"
+  },
+  {
+    "revision": "a6fb19279a8eb0f9a8fa30dc07df2e29",
+    "url": "/JustCleaning/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JustCleaning/robots.txt"
+  }
+]);
