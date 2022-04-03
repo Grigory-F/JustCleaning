a5ff120453f012f75ccceba37fef6de83cd87da6
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2022-04-03 18:41:39 +0300

    double cchange

---
+++ precache-manifest.424742ade07f833fc5b0f98a75139584.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ade38402b00a2ba2781",
+    "url": "/JustCleaning/css/app.74be7ff0.css"
+  },
+  {
+    "revision": "521aec237596fdca19a9",
+    "url": "/JustCleaning/css/chunk-vendors.f579dc58.css"
+  },
+  {
+    "revision": "11ef8997666b7fd7c353106152ee62a9",
+    "url": "/JustCleaning/img/kiska.11ef8997.jpg"
+  },
+  {
+    "revision": "166244abaf4174c80568b1f4766b046a",
+    "url": "/JustCleaning/index.html"
+  },
+  {
+    "revision": "2ade38402b00a2ba2781",
+    "url": "/JustCleaning/js/app.481c8b8d.js"
+  },
+  {
+    "revision": "521aec237596fdca19a9",
+    "url": "/JustCleaning/js/chunk-vendors.d9f5f378.js"
+  },
+  {
+    "revision": "a6fb19279a8eb0f9a8fa30dc07df2e29",
+    "url": "/JustCleaning/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/JustCleaning/robots.txt"
+  }
+]);
